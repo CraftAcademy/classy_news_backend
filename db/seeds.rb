fdd682b842bb6988a84bd80ed9a465138ed96572
David_Izosimov <David.Izosimov@gmail.com>
2019-11-06 15:36:13 +0100

    Adds .first to image attachment and updates seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,4 @@
 User.destroy_all
-Article.destroy_all
-user1 = User.create(email: 'user@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
-user2 = User.create(email: 'user2@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'journalist')
-user3 = User.create(email: 'user3@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'editor')
+user1 = User.create(email: 'user@mail.com', password: 'password', name: 'faraz', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
+user2 = User.create(email: 'user2@mail.com', password: 'password', name: 'john', city: 'Stockholm', country: 'Sweden', role: 'journalist')
+user3 = User.create(email: 'user3@mail.com', password: 'password', name: 'jane', city: 'Stockholm', country: 'Sweden', role: 'user')
